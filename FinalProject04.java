94b5662b8972379101b1c4d62f33ab387aec3094
ItsMyIP <bayudika004@gmail.com>
2025-09-25 08:51:55 +0700

    kdoe baris dua

--- FinalProject04.java
+++ FinalProject04.java
@@ -1,5 +1,6 @@
 public class FinalProject04 {
     public static void main(String[] args) {
         System.out.println("Hi, ini bari kode pertama");
+        System.out.println("hi, ini baris kode kedua");
     }
 }
