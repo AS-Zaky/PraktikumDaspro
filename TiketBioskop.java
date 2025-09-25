c999ec743bbf78c348eeb68a2536dade3dab189f
Zaky <donquixotevorosic@gmail.com>
2025-09-25 09:11:32 +0700

    Variabel

--- TiketBioskop.java
+++ TiketBioskop.java
@@ -4,6 +4,15 @@ public class TiketBioskop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
+        int hargaTiketDewasa = 50000;
+        int hargaTiketAnak = 30000;
+        int jumlahBeli, anak, dewasa;
+        double diskon;
+
+        System.out.println("Harga Tiket Bioskop\n Anak: 30.000\n Dewasa: 50000");
+        System.out.print("Beli berapa tiket anak: ");
+        anak = sc.nextInt();
+        System.out.println("Beli berapa tiket dewasa: ");
+        dewasa = sc.nextInt();
     }
 }
