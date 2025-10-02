34e23edf48866276fdbad1d22d96eb6480e9f658
Zaky <donquixotevorosic@gmail.com>
2025-10-02 09:58:01 +0700

    Tugas 2

---
+++ jobsheet5/genapganjil.java
@@ -0,0 +1,17 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class genapganjil {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Massukan Angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka "+angka+" adalah bilangan genap.");
+        } else {
+            System.out.println("Angka "+angka+" adalah bilangan ganjil.");
+        }
+    }
+}
