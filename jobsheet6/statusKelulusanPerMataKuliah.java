b34268cd9156f516f4b918ed29e990cac48174bf
Zaky <donquixotevorosic@gmail.com>
2025-10-09 08:38:33 +0700

    new File

---
+++ jobsheet6/statusKelulusanPerMataKuliah.java
@@ -0,0 +1,13 @@
+package jobsheet6;
+import java.util.Scanner;
+public class statusKelulusanPerMataKuliah {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("====== INPUT DATA MAHASISWA ======");
+    System.out.print("Nama\t\t: ");
+    String nama = sc.nextLine();
+    System.out.print("Nilai Akhir\t: ");
+    int nim = sc.nextInt()
+
+    
+}
