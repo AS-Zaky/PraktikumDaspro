72acd80f67178ac112a3eacd84842f33a471d422
Zaky <donquixotevorosic@gmail.com>
2025-10-30 08:02:32 +0700

    Square04 & Star04

--- FinalProject04.java
+++ FinalProject04.java
@@ -1,6 +1,12 @@
 public class FinalProject04 {
     public static void main(String[] args) {
-        System.out.println("Hi, ini bari kode pertama");
-        System.out.println("hi, ini baris kode kedua");
+        // System.out.println("Hi, ini bari kode pertama");
+        // System.out.println("hi, ini baris kode kedua");
+
+        int jajan = 10;
+
+        for (int i = 1; i <= jajan; i++) {
+            System.out.println("Ini Jajan ke "+ i);
+        }
     }
 }
